bc62214d152b0437ba0ac2d45becc4d064b2080a
Saurabh Shukla <103saurabhshukla@gmail.com>
2023-02-13 23:41:41 +0530

    Second commit

---
+++ Rotating 2-D array.cpp
@@ -0,0 +1,66 @@
+// Steps:
+// 1.	Swap a[i][j]==a[j][i]
+// 2.	Then interchange rows of array.
+// 3.	Here comes the solution.
+
+// Snippet:
+
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+
+class Solution
+{   
+    public:
+    //Function to rotate matrix anticlockwise by 90 degrees.
+    void rotateby90(vector<vector<int> >& matrix, int n) 
+    { 
+        // code here 
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        int l=0;
+        int h=n-1;
+        while(l<h){
+            swap(matrix[l],matrix[h]);
+            l++;
+            h--;
+        }
+    } 
+};
+
+
+
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--) 
+    {
+        int n;
+        cin>>n;
+        vector<vector<int> > matrix(n); 
+
+        for(int i=0; i<n; i++)
+        {
+            matrix[i].assign(n, 0);
+            for( int j=0; j<n; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        ob.rotateby90(matrix, n);
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < n; ++j)
+                cout<<matrix[i][j]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+
